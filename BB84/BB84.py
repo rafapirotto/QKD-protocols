37463fe9aba0d01df3c2aa087324050f076b97fa
Rafael Pirotto <pirotto@outlook.com>
2021-06-07 18:43:55 -0300

    Initial commit

---
+++ BB84/BB84.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from helpers import *
+
+print("\nBB84 without Eve\n")
+
+backend_input = input("Run using simulator?(y/n): ").lower()
+size = int(input("Enter desired length of bits: "))
+threshold = int(input("Enter desired threshold: "))
+shots = int(input("Enter desired number of shots: "))
+print("\n")
+
+backend = QasmSimulator()
+
+if backend_input == "n":
+    my_provider = IBMQ.load_account()
+    backend = my_provider.get_backend('ibmq_16_melbourne')
+
+alice_bits = get_random_sequence_of_bits(size)
+alice_bases = get_random_sequence_of_bases(size)
+
+# example:
+# alice_bits = ['0','1','0','0', '0', '1','0','1']
+# alice_bases = ['Z', 'Z', 'H', 'Z', 'Z', 'H', 'H', "H"]
+alice_states = get_states(alice_bits, alice_bases)
+
+circuit = QuantumCircuit(size, size)
+
+insert_states_in_circuit(circuit, alice_states)
+
+bob_bases = get_random_sequence_of_bases(size)
+# bob_bases = ['Z', 'Z', 'H', 'H', 'H', 'Z','H','H']
+
+make_measurements(bob_bases, circuit)
+
+counts = get_counts(circuit, backend, shots)
+
+key = get_key(counts, shots, threshold, size)
+
+
+print(f"\nAlice bits: {alice_bits}")
+print(f"Alice bases: {alice_bases}")
+print(f"Alice states: {alice_states}\n")
+print(f"Bob bases: {bob_bases}\n")
+print(f"Key: {key}\n")
+
+save_circuit_image(circuit, "bb84_circuit")
+
+
+# In[ ]:
+
+
+
+
---
+++ BB84/helpers.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, transpile
+from qiskit.providers.aer import QasmSimulator
+from qiskit.tools.monitor import job_monitor
+from qiskit.tools.visualization import circuit_drawer
+
+def get_random_sequence_of_bits(size):
+    bits = []
+    simulator = QasmSimulator()
+    circuit = QuantumCircuit(size, size)
+    
+    for i in range(size):
+        circuit.h([i])
+        circuit.measure([i], [i])
+
+    compiled_circuit = transpile(circuit, simulator)
+    job = simulator.run(compiled_circuit, shots=1)
+    job_monitor(job)
+    result = job.result()
+    counts = result.get_counts(circuit)
+    str_key = next(iter(counts))
+    
+    return list(str_key)
+
+def get_random_sequence_of_bases(size):
+    bit_sequence = get_random_sequence_of_bits(size)
+    bases = ['Z' if bit == '0' else 'H' for bit in bit_sequence]
+    
+    return bases
+
+def get_state(bit, base):
+    if bit == '0':
+        if base == 'Z':
+            return '|0>'
+        elif base == 'H':
+            return '|+>'
+    if bit == '1':
+        if base == 'Z':
+            return '|1>'
+        elif base == 'H':
+            return '|->'
+
+def get_states(bits, bases):
+    states = []
+    
+    for i in range(len(bits)):
+        state = get_state(bits[i], bases[i])
+        states.append(state)
+        
+    return states
+
+def insert_states_in_circuit(circuit, states):
+    for i in range(len(states)):
+        state = states[i]
+        
+        if state == '|1>':
+            circuit.x([i])
+        elif state == '|+>':
+            circuit.h([i])
+        elif state == '|->':
+            circuit.x([i])
+            circuit.h([i])
+            
+
+def measureInZ(circuit, i):
+     circuit.measure([i], [i])
+        
+def measureInH(circuit, i):
+    circuit.h([i])
+    measureInZ(circuit, i)
+
+def make_measurements(bob_bases, circuit):
+    for i in range(len(bob_bases)):
+        bob_base = bob_bases[i]
+        
+        if bob_base == 'Z':
+            measureInZ(circuit, i)
+        elif bob_base == 'H':
+            measureInH(circuit, i)
+            
+def get_key(counts, shots, threshold, size):
+    final_key = ''
+    value_list = counts.items()
+
+    for i in range(size):
+        zeros = 0
+        ones = 0
+
+        for index, (key, value) in enumerate(value_list):
+            if key[i] == '1':
+                ones += value
+            elif key[i] == '0':
+                zeros += value
+                
+        if ones > zeros:
+            if ones*100/shots >= threshold:
+                final_key += '1'
+        elif zeros > ones:
+            if zeros*100/shots >= threshold:
+                final_key += '0'
+                
+    return final_key
+
+def save_circuit_image(circuit, file_name):
+    diagram = circuit_drawer(circuit, output='mpl', style={'backgroundcolor': '#EEEEEE'})
+    diagram.savefig(f"{file_name}.png", format="png")
+    
+def get_counts(circuit, backend, shots):
+    compiled_circuit = transpile(circuit, backend)
+    job = backend.run(compiled_circuit, shots=shots)
+    job_monitor(job)
+    result = job.result()
+    counts = result.get_counts(circuit)
+    
+    return counts
+
+
+# In[ ]:
+
+
+
+
