62e121128f45ccd4000a36423a788d5a09f43a1d
Rafael Pirotto <pirotto@outlook.com>
2021-06-08 19:34:28 -0300

    Updated code

--- BB84/helpers.py
+++ BB84/helpers.py
@@ -1,13 +1,13 @@
-from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, transpile
+from qiskit import QuantumCircuit, transpile, IBMQ
 from qiskit.providers.aer import QasmSimulator
 from qiskit.tools.monitor import job_monitor
 from qiskit.tools.visualization import circuit_drawer
 
+
 def get_random_sequence_of_bits(size):
-    bits = []
     simulator = QasmSimulator()
     circuit = QuantumCircuit(size, size)
-    
+
     for i in range(size):
         circuit.h([i])
         circuit.measure([i], [i])
@@ -17,102 +17,129 @@ def get_random_sequence_of_bits(size):
     job_monitor(job)
     result = job.result()
     counts = result.get_counts(circuit)
-    str_key = next(iter(counts))
-    
-    return list(str_key)
+    str_sequence = next(iter(counts))
+
+    return list(str_sequence)
+
 
 def get_random_sequence_of_bases(size):
     bit_sequence = get_random_sequence_of_bits(size)
-    bases = ['Z' if bit == '0' else 'H' for bit in bit_sequence]
-    
+    bases = ['Z' if bit == '0' else 'X' for bit in bit_sequence]
+
     return bases
+
 
 def get_state(bit, base):
     if bit == '0':
         if base == 'Z':
             return '|0>'
-        elif base == 'H':
+        elif base == 'X':
             return '|+>'
     if bit == '1':
         if base == 'Z':
             return '|1>'
-        elif base == 'H':
+        elif base == 'X':
             return '|->'
+
 
 def get_states(bits, bases):
     states = []
-    
+
     for i in range(len(bits)):
         state = get_state(bits[i], bases[i])
         states.append(state)
-        
+
     return states
+
 
 def insert_states_in_circuit(circuit, states):
     for i in range(len(states)):
         state = states[i]
-        
-        if state == '|1>':
+
+        if state == '|0>':
+            pass
+        elif state == '|1>':
             circuit.x([i])
         elif state == '|+>':
             circuit.h([i])
         elif state == '|->':
             circuit.x([i])
             circuit.h([i])
-            
 
-def measure_in_Z(circuit, i):
-     circuit.measure([i], [i])
-        
-def measure_in_H(circuit, i):
+
+def measure_in_z(circuit, i):
+    circuit.measure([i], [i])
+
+
+def measure_in_x(circuit, i):
     circuit.h([i])
-    measure_in_Z(circuit, i)
+    measure_in_z(circuit, i)
 
-def make_measurements(bob_bases, circuit):
-    for i in range(len(bob_bases)):
-        bob_base = bob_bases[i]
-        
-        if bob_base == 'Z':
-            measure_in_Z(circuit, i)
-        elif bob_base == 'H':
-            measure_in_H(circuit, i)
-            
-def get_key(counts, shots, threshold, size):
+
+def make_measurements(bases, circuit):
+    for i in range(len(bases)):
+        base = bases[i]
+
+        if base == 'Z':
+            measure_in_z(circuit, i)
+        elif base == 'X':
+            measure_in_x(circuit, i)
+
+
+def get_same_bases_positions(first_bases, second_bases):
+    positions = []
+    bases_length = len(first_bases)
+
+    for i in range(bases_length):
+        first_base = first_bases[i]
+        second_base = second_bases[i]
+
+        if first_base == second_base:
+            positions.append(bases_length - 1 - i)
+
+    positions.reverse()
+
+    return positions
+
+
+def get_key(counts, shots, threshold, valid_positions):
     final_key = ''
     value_list = counts.items()
 
-    for i in range(size):
+    for i in valid_positions:
         zeros = 0
         ones = 0
 
-        for index, (key, value) in enumerate(value_list):
+        for (key, value) in value_list:
             if key[i] == '1':
                 ones += value
             elif key[i] == '0':
                 zeros += value
-                
+
         if ones > zeros:
-            if ones*100/shots >= threshold:
+            if ones * 100 / shots >= threshold:
                 final_key += '1'
         elif zeros > ones:
-            if zeros*100/shots >= threshold:
+            if zeros * 100 / shots >= threshold:
                 final_key += '0'
-                
+
     return final_key
+
 
 def save_circuit_image(circuit, file_name):
     diagram = circuit_drawer(circuit, output='mpl', style={'backgroundcolor': '#EEEEEE'})
     diagram.savefig(f"{file_name}.png", format="png")
-    
+
+
 def get_counts(circuit, backend, shots):
     compiled_circuit = transpile(circuit, backend)
     job = backend.run(compiled_circuit, shots=shots)
     job_monitor(job)
     result = job.result()
     counts = result.get_counts(circuit)
-    
+
     return counts
 
 
-
-
+def privacy_amplification():
+    return None
--- BB84/main.py
+++ BB84/main.py
@@ -2,49 +2,46 @@ from helpers import *
 
 print("\nBB84 without Eve\n")
 
-backend_input = input("Run using simulator?(y/n): ").lower()
+use_simulator = input("Run using simulator?(y/n): ").lower()
 size = int(input("Enter desired length of bits: "))
-threshold = int(input("Enter desired threshold: "))
-shots = int(input("Enter desired number of shots: "))
-print("\n")
-
+threshold = 100
+shots = 1
 backend = QasmSimulator()
 
-if backend_input == "n":
+if use_simulator == "n":
     my_provider = IBMQ.load_account()
     backend = my_provider.get_backend('ibmq_16_melbourne')
+    shots = int(input("Enter desired number of shots: "))
+    threshold = int(input("Enter desired threshold: "))
+
+print("\n")
 
 alice_bits = get_random_sequence_of_bits(size)
 alice_bases = get_random_sequence_of_bases(size)
-
-# example:
 # alice_bits = ['0','1','0','0', '0', '1','0','1']
-# alice_bases = ['Z', 'Z', 'H', 'Z', 'Z', 'H', 'H', "H"]
+# alice_bases = ['Z', 'Z', 'X', 'Z', 'Z', 'X', 'X', "X"]
 alice_states = get_states(alice_bits, alice_bases)
+
+bob_bases = get_random_sequence_of_bases(size)
+# bob_bases = ['Z', 'Z', 'X', 'X', 'X', 'Z','X','X']
 
 circuit = QuantumCircuit(size, size)
 
 insert_states_in_circuit(circuit, alice_states)
 
-bob_bases = get_random_sequence_of_bases(size)
-# bob_bases = ['Z', 'Z', 'H', 'H', 'H', 'Z','H','H']
-
 make_measurements(bob_bases, circuit)
 
 counts = get_counts(circuit, backend, shots)
 
-key = get_key(counts, shots, threshold, size)
+same_bases_positions = get_same_bases_positions(alice_bases, bob_bases)
 
+key = get_key(counts, shots, threshold, same_bases_positions)
 
 print(f"\nAlice bits: {alice_bits}")
 print(f"Alice bases: {alice_bases}")
 print(f"Alice states: {alice_states}\n")
 print(f"Bob bases: {bob_bases}\n")
+print(f"Same bases positions: {same_bases_positions}\n")
 print(f"Key: {key}\n")
 
 save_circuit_image(circuit, "bb84_circuit")
-
-
-
-
-
